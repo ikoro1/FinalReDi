060d6da878ad419c5d1a7dea4f2761b1c9258d08
Igor Korobov <igor.korobov@dci-student.de>
2023-12-04 19:23:32 +0100

    test with flask

--- 4.py
+++ 4.py
@@ -73,4 +73,4 @@ def index():
     return render_template('index4.html', result=result, error=error)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5003)
+    app.run(debug=True, port=5004)
